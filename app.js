43cb527be23659c5f56462cf55c8148dd7833f35
arcsurf <ariel.couso@gmail.com>
2021-07-29 18:25:34 -0300

    Se agrega el Procfile al root del proyecto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
-const PORT = 3000
+const port = 3000;
 
 app.use(express.static(publicPath));
 
@@ -18,6 +18,6 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.port || 3000, function() {
     console.log("El servidor se encuentra activo sobre la URL: http://localhost:" + port + " o sobre http://127.0.0.1:" + port)
 })
