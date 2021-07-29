99b9077881a28a5b1695e7dc09b61a4d2b1e06ba
arcsurf <ariel.couso@gmail.com>
2021-07-29 17:44:43 -0300

    Se modifica el puerto del server al 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
-const port = 3030
+const port = 3000
 
 app.use(express.static(publicPath));
 
